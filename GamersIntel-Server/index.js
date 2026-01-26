c13ac5d73ddd031424b0b83cb9d8f8a4634eb731
abdullahalnoman003 <noman22622@gmail.com>
2026-01-27 03:02:08 +0600

    deployed to vercel and removed legacy code that is failing to run on localhost on my computer.

--- GamersIntel-Server/index.js
+++ GamersIntel-Server/index.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ko3ml0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-xxxx-shard-00-00.mongodb.net:27017,ac-xxxx-shard-00-01.mongodb.net:27017,ac-xxxx-shard-00-02.mongodb.net:27017/?ssl=true&replicaSet=atlas-xxxx-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,10 +22,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
 
-
-    
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
@@ -32,6 +29,15 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("GameStack is running");
+});
+
+app.get('/health', async (req, res) => {
+    try {
+        await client.db("admin").command({ ping: 1 });
+        res.json({ status: 'healthy', database: 'connected' });
+    } catch (error) {
+        res.status(503).json({ status: 'unhealthy', database: 'disconnected', error: error.message });
+    }
 });
 
 app.listen(port, () =>{
